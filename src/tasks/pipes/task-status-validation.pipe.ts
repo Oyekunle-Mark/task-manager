fca59c35855420ef9fada24db6820d6143fd4e9a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-20 21:22:27 +0100

    Added a custom validation for the status update handler

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,7 +1,23 @@
-import {PipeTransform} from '@nestjs/common';
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { TaskStatus } from '../tasks.model';
 
 export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStatus = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE,
+    ];
+
     transform(value: any) {
-        
+        value = value.toUpperCase();
+
+        if (!this.isValidStatus(value)) {
+            throw new BadRequestException(`"${value}" is an invalid status`);
+        }
+    }
+
+    private isValidStatus(status: any): boolean {
+        const idx = this.allowedStatus.indexOf(status);
+        return idx !== -1;
     }
 }
