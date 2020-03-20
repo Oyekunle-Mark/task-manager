9a92151f02d1ad24ec65962de235ae47d15b1ea1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-20 08:25:35 +0100

    Handler to delete a task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -24,5 +24,12 @@ export class TasksController {
         @Body() createTaskDto: CreateTaskDto
     ): Task {
         return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    deleteTask(
+        @Param('id') id: string
+    ) {
+        return this.tasksService.deleteTask(id);
     }
 }
