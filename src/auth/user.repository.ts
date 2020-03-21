0554faa8a62c3ff67205ea004758d30ee680ed4d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-21 21:01:57 +0100

    Hashes the password before saving

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -7,12 +7,13 @@ import { ConflictException, InternalServerErrorException } from "@nestjs/common"
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        const {username, password} = authCredentialsDto;
+        const { username, password } = authCredentialsDto;
 
         const user = new User();
         user.username = username;
-        user.password = password;
-        
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
+
         try {
             await user.save();
         } catch (error) {
@@ -25,7 +26,6 @@ export class UserRepository extends Repository<User> {
     }
 
     private async hashPassword(password: string, salt: string): Promise<string> {
-        return bcrypt.hash(password,salt);
+        return bcrypt.hash(password, salt);
     }
 }
- 
