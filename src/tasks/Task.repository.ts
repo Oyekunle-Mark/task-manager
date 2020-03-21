32faa9234f443021367df543e29eaf742c917b98
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-21 02:07:22 +0100

    Craeted a creteTask repository

--- src/tasks/Task.repository.ts
+++ src/tasks/Task.repository.ts
@@ -1,7 +1,19 @@
 import { Repository, EntityRepository } from "typeorm";
 import { TaskEntity } from "./task.entity";
+import { TaskStatus } from "./task-status.enum";
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 @EntityRepository(TaskEntity)
 export class TaskRepository extends Repository<TaskEntity> {
-    
+    async createTask(createTaskDto: CreateTaskDto): Promise<TaskEntity> {
+        const { title, description } = createTaskDto;
+
+        const task = new TaskEntity();
+        task.title = title;
+        task.description = description;
+        task.status = TaskStatus.OPEN;
+        await task.save();
+
+        return task;
+    }
 }
