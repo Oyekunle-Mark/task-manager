52a1999bcbdb7a5967a1cc67ea4cb7cccc10a79a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-21 21:56:57 +0100

    Created and used the jwt payload interface

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload-interface';
 
 @Injectable()
 export class AuthService {
@@ -16,14 +17,14 @@ export class AuthService {
         return this.userRepository.signUp(authCredentialsDto);
     }
 
-    async signIn(authCredentialsDto: AuthCredentialsDto) {
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
         const username = await this.userRepository.validateUserPassword(authCredentialsDto)
 
         if (!username) {
             throw new UnauthorizedException('Invalid credentials')
         }
 
-        const payload = { username };
+        const payload: JwtPayload = { username };
         const accessToken = await this.jwtService.sign(payload);
 
         return { accessToken };
