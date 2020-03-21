8ca9a69777a9a63f2f2b54006d791a61cd6ab9aa
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-21 21:54:16 +0100

    Signs and returns a token upon validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository
+        private userRepository: UserRepository,
+        private jwtService: JwtService,
     ) { }
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
@@ -15,10 +17,15 @@ export class AuthService {
     }
 
     async signIn(authCredentialsDto: AuthCredentialsDto) {
-        const username = await this.userRepository.validateUserPassword (authCredentialsDto)
+        const username = await this.userRepository.validateUserPassword(authCredentialsDto)
 
         if (!username) {
             throw new UnauthorizedException('Invalid credentials')
         }
+
+        const payload = { username };
+        const accessToken = await this.jwtService.sign(payload);
+
+        return { accessToken };
     }
 }
