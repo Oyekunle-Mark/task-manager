f58c2f280782618e339c59d0086f29da94edf185
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-23 18:59:40 +0100

    Gets task or handles the error nicely with logging and a nice exceptio

--- src/tasks/Task.repository.ts
+++ src/tasks/Task.repository.ts
@@ -4,9 +4,12 @@ import { TaskStatus } from "./task-status.enum";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 import { User } from "../auth/user.entity";
+import { Logger, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+    private logger = new Logger('TaskRepository');
+
     async getTasks(
         filterDto: GetTasksFilterDto,
         user: User,
@@ -24,8 +27,13 @@ export class TaskRepository extends Repository<Task> {
             query.andWhere('task.title LIKE :search OR task.description LIKE :search', { search: `%${search}%` })
         }
 
-        const tasks = await query.getMany();
-        return tasks;
+        try {
+            const tasks = await query.getMany();
+            return tasks;
+        } catch (error) {
+            this.logger.error(`Failed to get task for user "${user.username}". Filters: ${JSON.stringify(filterDto)}`, error.stack);
+            throw new InternalServerErrorException();
+        }
     }
 
     async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
