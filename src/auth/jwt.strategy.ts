38e2d0e97a82db73cc07dc03ff90d3eb2cd132a2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-23 14:30:35 +0100

    Created the user repository

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,13 +1,31 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtPayload } from './jwt-payload-interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: UserRepository,
+    ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: 'VeryPrivateKeyHere',
         });
+    }
+
+    async validate(payload: JwtPayload): Promise<User> {
+        const { username } = payload;
+        const user = await this.userRepository.findOne({ username });
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+
+        return user;
     }
 }
