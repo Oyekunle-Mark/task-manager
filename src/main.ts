5363e8781da00bbadbabc5bcfa24b1f5e581366f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-23 18:31:59 +0100

    Changes the formatting of the code

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);
-
   const PORT = 3000;
+
+  const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
+
   logger.log(`Application started on port ${PORT}`);
 }
 bootstrap();
