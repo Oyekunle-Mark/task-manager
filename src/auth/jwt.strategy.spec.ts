c791468e5ac4c73b69f0c0ca906b3f5b290f6005
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-24 22:55:31 +0100

    A before each to create testing module

--- src/auth/jwt.strategy.spec.ts
+++ src/auth/jwt.strategy.spec.ts
@@ -2,6 +2,23 @@ import { Test } from '@nestjs/testing';
 import { JwtStrategy } from './jwt.strategy';
 import { UserRepository } from './user.repository';
 
-describe('JwtStrategy', () => {
+const mockUserRepository = () => ({
+    findOne: jest.fn(),
+})
 
+describe('JwtStrategy', () => {
+    let jwtStrategy: JwtStrategy;
+    let userRepository;
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [
+                JwtStrategy,
+                { provide: UserRepository, useFactory: mockUserRepository },
+            ]
+        }).compile();
+
+        jwtStrategy = await module.get<JwtStrategy>(JwtStrategy);
+        userRepository = await module.get<UserRepository>(UserRepository);
+    });
 });
