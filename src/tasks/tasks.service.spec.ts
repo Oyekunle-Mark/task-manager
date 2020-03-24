4f8782c4b27da73b84ac9f0695fa21cd33af7943
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-24 20:28:00 +0100

    Mocked the return value of gettakss

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -27,12 +27,16 @@ describe('TasksService', () => {
     });
 
     describe('getTasks', () => {
-        it('gets all tasks from the repository', () => {
+        it('gets all tasks from the repository', async () => {
+            taskRepository.getTasks.mockResolvedValue('someValue')
+
             expect(taskRepository.getTasks).not.toHaveBeenCalled();
 
             const filters: GetTasksFilterDto = { status: TaskStatus.IN_PROGRESS, search: 'Some search query' };
-            tasksService.getTasks(filters, mockUser);
+            const result = await tasksService.getTasks(filters, mockUser);
+
             expect(taskRepository.getTasks).toHaveBeenCalled();
+            expect(result).toEqual('someValue');
         });
 
     })
