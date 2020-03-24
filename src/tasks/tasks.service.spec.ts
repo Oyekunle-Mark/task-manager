a0b388b6f4f2dc698206c0aaa0ce82a4ad74feb9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-24 20:10:53 +0100

    Factory for mocked repository

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { TaskRepository } from './task.repository';
 
 const mockTaskRepository = () => ({
 
@@ -13,9 +14,11 @@ describe('TasksService', () => {
         const module = await Test.createTestingModule({
             providers: [
                 TasksService,
+                { provide: TaskRepository, useFactory: mockTaskRepository }
             ]
         }).compile();
 
         tasksService = await module.get<TasksService>(TasksService);
+        taskRepository = await module.get<TaskRepository>(TaskRepository);
     });
 });
