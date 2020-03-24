c278205b7c76a776afe91b53bd3b991d66c31b02
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-24 21:48:12 +0100

    Creates a testing module of the user repository

---
+++ src/auth/user.repository.spec.ts
@@ -0,0 +1,17 @@
+import { Test } from "@nestjs/testing";
+import { UserRepository } from "./user.repository";
+
+
+describe('UserRepository', () => {
+    let userRepository;
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [
+                UserRepository,
+            ],
+        }).compile();
+
+        userRepository = await module.get<UserRepository>(UserRepository);
+    });
+});
