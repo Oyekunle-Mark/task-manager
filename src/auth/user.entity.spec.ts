87c5c8e1eb373e239658257934d8170b4970ca79
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-24 22:37:56 +0100

    UserEntity test block

---
+++ src/auth/user.entity.spec.ts
@@ -0,0 +1,11 @@
+describe('UserEntity', () => {
+    describe('validatePassword', () => {
+        it('returns true as password is valid', () => {
+
+        });
+
+        it('returns false as password is not valid', () => {
+
+        });
+    });
+});
