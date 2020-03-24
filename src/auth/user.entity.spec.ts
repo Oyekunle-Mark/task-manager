033ce6cd52c9bc417399aea15f77c68c579d80e5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-24 22:41:40 +0100

    Mocked the user methods

--- src/auth/user.entity.spec.ts
+++ src/auth/user.entity.spec.ts
@@ -1,4 +1,15 @@
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
 describe('UserEntity', () => {
+    let user: User;
+
+    beforeEach(() => {
+        user = new User();
+        user.password = 'testPassword';
+        user.salt = 'testSalt';
+        bcrypt.hash = jest.fn();
+    });
     describe('validatePassword', () => {
         it('returns true as password is valid', () => {
 
